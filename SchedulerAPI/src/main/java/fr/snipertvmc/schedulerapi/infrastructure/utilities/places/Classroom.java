2280c6555c8c47795dd277a661342873d9390ec2
Sniper_TVmc <snipertv59.pro@gmail.com>
2024-09-04 22:33:03 +0200

    Added getters/setters and new value "floor"

--- SchedulerAPI/src/main/java/fr/snipertvmc/schedulerapi/infrastructure/utilities/places/Classroom.java
+++ SchedulerAPI/src/main/java/fr/snipertvmc/schedulerapi/infrastructure/utilities/places/Classroom.java
@@ -13,6 +13,8 @@ public class Classroom {
 	private final UUID uuid;
 
 	private Building building;
+	private byte floor;
+
 	private String displayName;
 	private byte maxCapacity;
 	private ClassroomType type;
@@ -32,4 +34,74 @@ public class Classroom {
 
 
 	// -------------------------------------------------- //
+
+
+	public Building getBuilding() {
+		return building;
+	}
+
+
+	public Classroom setBuilding(Building building) {
+        this.building = building;
+        return this;
+    }
+
+
+	// -------------------------------------------------- //
+
+
+    public byte getFloor() {
+        return floor;
+    }
+
+
+    public Classroom setFloor(byte floor) {
+        this.floor = floor;
+        return this;
+    }
+
+
+	// -------------------------------------------------- //
+
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+
+    public Classroom setDisplayName(String displayName) {
+        this.displayName = displayName;
+        return this;
+    }
+
+
+	// -------------------------------------------------- //
+
+
+	public byte getMaxCapacity() {
+        return maxCapacity;
+    }
+
+
+	public Classroom setMaxCapacity(byte maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        return this;
+    }
+
+
+	// -------------------------------------------------- //
+
+
+	public ClassroomType getType() {
+        return type;
+    }
+
+
+	public Classroom setType(ClassroomType type) {
+        this.type = type;
+        return this;
+    }
+
+
+    // -------------------------------------------------- //
 }
