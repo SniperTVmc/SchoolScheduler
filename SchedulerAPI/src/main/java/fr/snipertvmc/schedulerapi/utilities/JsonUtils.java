87ecbf28a782e82e451899a6963c90506daee081
Sniper_TVmc <snipertv59.pro@gmail.com>
2024-09-08 14:29:38 +0200

    [⚡] Update JsonUtils class, and new method to load json from a file

--- SchedulerAPI/src/main/java/fr/snipertvmc/schedulerapi/utilities/JsonUtils.java
+++ SchedulerAPI/src/main/java/fr/snipertvmc/schedulerapi/utilities/JsonUtils.java
@@ -3,30 +3,51 @@ package fr.snipertvmc.schedulerapi.utilities;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.snipertvmc.schedulerapi.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class JsonUtils {
 
 
-	private static ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 
 	// -------------------------------------------------- //
 
 
-	public static String objectToJson(Object object) {
+	public static String mapToJson(Map<String, Object> map) {
 		try {
-			return objectMapper.writeValueAsString(object);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
+			return objectMapper.writeValueAsString(map);
+		} catch (JsonProcessingException exception) {
+			throw new RuntimeException(exception);
 		}
 	}
 
 
-	public static Object jsonToObject(String json) {
+	public static Map<String, Object> jsonToMap(String json) {
 		try {
 			return objectMapper.readValue(json, new TypeReference<>(){});
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
+		} catch (JsonProcessingException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+
+	// -------------------------------------------------- //
+
+
+	public static Map<String, Object> loadJson(String filePath) {
+		try (InputStream inputStream = Main.class.getResourceAsStream("/" + filePath)) {
+			if (inputStream == null) {
+				throw new IOException("File not found: " + filePath);
+			}
+			return objectMapper.readValue(inputStream, new TypeReference<>() {});
+		} catch (IOException exception) {
+			throw new RuntimeException("Error loading JSON file", exception);
 		}
 	}
 
